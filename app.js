665f0f6a1df22e1fa1c7baa733720f8256ec51c6
Doan Ngoai Giao <ngoaigiao07@gmail.com>
2023-02-16 17:14:07 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const res = require("express/lib/response");
+//const res = require("express/lib/response");
+
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/contacts", contactsRouter);
+    
 app.get("/", (req, res) =>{
     res.json({ message: "Welcome to contact book application." });
 });
+
 
 module.exports = app;
